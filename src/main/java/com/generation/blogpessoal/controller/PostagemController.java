1b9bf4303c572d01e51e45f4757bf8574662e8d3
Luana-Nascimento <luananascimento00@outlook.com>
2024-05-22 12:23:42 -0300

    Atualização com CRUD

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,16 +1,26 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+
+import jakarta.validation.Valid;
 
 @RestController //anotação que diz para spring que essa é uma controladora de rotas e acesso aos metodos
 @RequestMapping("/postagens") //rota para chegar nessa classe (insomnia)
@@ -27,6 +37,53 @@ public class PostagemController {
 		//SELECT * FROM tb_postagens
 	
 	}
+	//O id está entre chaves, pois não será inserido no local host diretamente o id e sim o que foi atribuido a ele. Ex: localhost:8080/postagens/2  
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		// findById = SELECT * FROM tb_postagens WHERE id = 1;
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", "2024-12-31 14:05:01");
+		@PostMapping
+		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+			//retorno em formato ResponseEntity
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
+	}
+		//atualizando dado do banco de dados através do Id
+		@PutMapping
+		 //o nome do método como put foi definido, mas poderia ser qualquer outra palavra.
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+			return postagemRepository.findById(postagem.getId())
+					.map(resposta-> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem)))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());}
+		
 
-
+		//DELETE FROM tb_postagens WHERE id = id;
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);
+	}
 }
+		
+		// Get - recuperar as informações do banco de dados
+		// Post - insert no banco de dados
+		// Put - update no banco de dados
+		// Delete - deleta algum registro do banco de dados
+
+
+
+
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,19 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.generation.blogpessoal.model.Postagem;
 
 //JpaRepository - classe do JPA (dependência adicionada), dentro dela contém métodos que vão realizar Query no banco. Significa que a JPA assume o controle de criar automaticamente o SELECT * FROM tb_postagem. 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 // entre o diamante se coloca a classe (postagem) que está puxando da model e o tipo (long). 
+	// a JPA faz a ligação para buscar no banco de dados
+	
+	//SELECT * FROM tb_postagens WHERE titulo LIKE "%POST%"; = findAllByTituloContainingIgnoreCase
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+
 }
